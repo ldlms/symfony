e6341011b936728e799a7a27abf2b8cbd38bacbd
leo <leodelmas31@gmail.com>
2023-04-14 16:31:16 +0200

    vendredi14

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,20 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use App\Entity\Article;
+use App\Form\ArticleType;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
-    {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
-    }
 
     #[Route('/article/all', name: 'app_article_all')]
     public function showArticle(ArticleRepository $articleRepository):Response{
@@ -25,7 +20,33 @@ class ArticleController extends AbstractController
         // dd($articles);
         return $this->render('article/index2.html.twig', [
             'liste' => $articles
+            // 'liste' => $articleRepository->findAll(); (marche aussi)
             // 'filmes'=> $article,
+        ]);
+    }
+
+    #[Route('/article/id/{id}', name: 'app_article_id')]
+    public function showArticleById(ArticleRepository $articleRepository, $id):Response{
+
+        $articles = $articleRepository->find($id);
+
+
+        return $this->render('article/index.html.twig', [
+            'article' => $articles,
+        ]);
+        // recuperer l'article selon son id
+        // faire un twig
+    }
+
+    #[Route('/article/add', name:'app_article_add')]
+    public function addArticle():Response{
+        
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+    
+        return $this->render('article/articleAdd.html.twig', [
+            'form'=> $form->createView(),
         ]);
     }
 }
